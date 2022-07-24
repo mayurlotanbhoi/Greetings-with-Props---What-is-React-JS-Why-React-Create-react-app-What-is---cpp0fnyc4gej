df2c8b90e9b27a079c2fa7a8c6ab8159e662c5ae
mayur bhoi <101246469+mayurlotanbhoi@users.noreply.github.com>
2022-07-24 15:50:13 +0530

    add welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+
+function Welcome(props){
+    let name = props.name;
+
+    return<>
+    <h1>Hey ! ${name}</h1>
+    <h2>Welcome to Newton School</h2>
+    
+    </>
+}
+
+export default Welcome;
