818d68ed880dd4183c974cbd3e37b59474c9a674
mayur bhoi <101246469+mayurlotanbhoi@users.noreply.github.com>
2022-07-28 13:04:47 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,11 @@ function Welcome(props){
     let student = props.name;
 
     return(
-    <h1>Hey ! {student} <h2>Welcome to Newton School</h2></h1>
-    
+        <>
+    <h1>Hey ! {student} </h1>
+        <h2>Welcome to Newton School</h2>
+        
+    </>
     )
     
 }
