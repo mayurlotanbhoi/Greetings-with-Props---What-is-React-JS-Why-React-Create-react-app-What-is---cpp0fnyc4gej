81eaa44154e3fc24113739771bb3134b49beb905
mayur bhoi <mayurbhoi200@gmail.com>
2022-08-12 21:21:26 +0530

    add name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,15 @@
 // write code for Welcome component here
-import React from "react";
+import React from 'react';
 
-function Welcome(props) {
-  return (
-    <>
-      <h1>
-        Hey ! {props.name} 
-      </h1>
-    
-    <h2> Welcome to Newton School </h2>
-    </>
-  );
-}
+const Welcome = ({name}) => {
+    let sayHi = `Hey ${name}!`;
+    let greetMessage = `Welcome to Newton School.`;
+    return (
+        <>
+            <h1>{sayHi}</h1>
+            <h2>{greetMessage}</h2>
+        </>
+    );
+};
 
 export default Welcome;
