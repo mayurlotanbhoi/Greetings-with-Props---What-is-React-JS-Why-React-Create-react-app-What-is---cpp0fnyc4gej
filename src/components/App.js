107c2e3825f349b3c98faaaa20fe922d8b07f058
mayurlotanbhoi <mayurbhoi200@gmail.com>
2022-08-12 13:16:13 +0530

    formattin push

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import "../styles/App.css";
 import Welcome from "./Welcome";
 
 const App = () => {
-    return <Welcome name = { "mayur" }
-    />;
+  return (
+    <>
+      <Welcome name="mayur" />
+    </>
+  );
 };
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,13 @@
 // write code for Welcome component here
 
 function Welcome(props) {
-    let student = props.name;
-
-    return (
-
-        <
-        >
-        <
-        h1 > Hey!{ student } < h2 > Welcome to Newton School < /h2></h
-        1 >
-        <
-        />
-
-    );
+  return (
+    <>
+      <h1>
+        Hey!{props.name} <h2> Welcome to Newton School </h2>
+      </h1>
+    </>
+  );
 }
 
 export default Welcome;
